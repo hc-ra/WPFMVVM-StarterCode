2d6b777d5d37c200081cc57577edb36469b4f484
Bartosz Migacz <4rchers@gmail.com>
2015-08-13 17:55:34 +0200

    modified to use MVVM

--- ZzaDashboard/ZzaDashboard/Customers/CustomerEditView.xaml.cs
+++ ZzaDashboard/ZzaDashboard/Customers/CustomerEditView.xaml.cs
@@ -23,33 +23,33 @@ namespace ZzaDashboard.Customers
 	/// </summary>
 	public partial class CustomerEditView : UserControl
 	{
-		private ICustomersRepository _repository = new CustomersRepository();
-		private Customer _customer = null;
+		//private ICustomersRepository _repository = new CustomersRepository();
+		//private Customer _customer = null;
 		public CustomerEditView()
 		{
 			InitializeComponent();
 		}
-		public Guid CustomerId
-		{
-			get { return (Guid)GetValue(CustomerIdProperty); }
-			set { SetValue(CustomerIdProperty, value); }
-		}
+	//	public Guid CustomerId
+	//	{
+	//		get { return (Guid)GetValue(CustomerIdProperty); }
+	//		set { SetValue(CustomerIdProperty, value); }
+	//	}
 
-		public static readonly DependencyProperty CustomerIdProperty =
-			DependencyProperty.Register("CustomerId", typeof(Guid),
-			typeof(CustomerEditView), new PropertyMetadata(Guid.Empty));
+	//	public static readonly DependencyProperty CustomerIdProperty =
+	//		DependencyProperty.Register("CustomerId", typeof(Guid),
+	//		typeof(CustomerEditView), new PropertyMetadata(Guid.Empty));
 
-		private async void OnLoaded(object sender, RoutedEventArgs e)
-		{
-			if (DesignerProperties.GetIsInDesignMode(this)) return;
+	//	private async void OnLoaded(object sender, RoutedEventArgs e)
+	//	{
+	//		if (DesignerProperties.GetIsInDesignMode(this)) return;
 
-			_customer = await _repository.GetCustomerAsync(CustomerId);
-			DataContext = _customer;
-		}
+	//		_customer = await _repository.GetCustomerAsync(CustomerId);
+	//		DataContext = _customer;
+	//	}
 
-		private async void OnSave(object sender, RoutedEventArgs e)
-		{
-			await _repository.UpdateCustomerAsync(_customer);
-		}
+	//	private async void OnSave(object sender, RoutedEventArgs e)
+	//	{
+	//		await _repository.UpdateCustomerAsync(_customer);
+	//	}
 	}
 }
---
+++ ZzaDashboard/ZzaDashboard/Customers/CustomerEditViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Zza.Data;
+using ZzaDashboard.Services;
+
+namespace ZzaDashboard.Customers
+{
+	public class CustomerEditViewModel : INotifyPropertyChanged
+	{
+		private Customer _customer;
+		private ICustomersRepository _repository = new CustomersRepository();
+
+		public CustomerEditViewModel()
+		{
+			SaveCommand = new RelayCommand(OnSave);
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+		public Customer Customer
+		{
+			get { return _customer; }
+			set
+			{
+				if (value != _customer)
+				{
+					_customer = value;
+					PropertyChanged(this, new PropertyChangedEventArgs("Customer"));
+				}
+			}
+		}
+
+		public Guid CustomerID { get; set; }
+		public ICommand SaveCommand { get; set; }
+
+		public async void LoadCustomer()
+		{
+			Customer = await _repository.GetCustomerAsync(CustomerID);
+		}
+
+		private async void OnSave()
+		{
+			Customer = await _repository.UpdateCustomerAsync(Customer);
+		}
+	}
+}
